a98c875fbe8a42aa03485dfd1cef6660ceccbe9c
DenisKazmirchuk <161360416+DenisKazmirchuk@users.noreply.github.com>
2024-12-10 20:40:25 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 32, 22, 55, 48, 26, 54, 98 };
+
+        var selectedNumbers = numbers.Skip(2).Take(4).ToArray();
+        Console.WriteLine("Вибрані числа:");
+        Console.WriteLine(string.Join(", ", selectedNumbers));
+    }
+}
